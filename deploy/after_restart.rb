647b6ad83c55b452b8f6ae124613afc03dcf2024
Jason Hansen <jason@slack.io>
2012-03-17 18:33:48 -0700

    noop the hooks

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,5 +1,5 @@
-on_app_master do
-  # Notify Campfire of deploy with link to diff
-  cmd = %{ruby -e "puts 'hello after_restart'" }
-  run cmd
-end
+#on_app_master do
+#  # Notify Campfire of deploy with link to diff
+#  cmd = %{ruby -e "puts 'hello after_restart'" }
+#  run cmd
+#end
